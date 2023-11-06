585a407ab415b30d6f14dbc1e446ed5b80452451
Shynoo <146827279+Shyno0@users.noreply.github.com>
2023-11-06 18:11:08 +0100

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,119 +1,51 @@
-//*******************************************************************************//
-// Association des entrées du L298N, aux sorties utilisées sur notre Arduino Uno //
-//*******************************************************************************//
-#define borneENA        10      // On associe la borne "ENA" du L298N à la pin D10 de l'Arduino (PWM possible sur cette broche)
-#define borneIN1        9       // On associe la borne "IN1" du L298N à la pin D9 de l'Arduino
-#define borneIN2        8       // On associe la borne "IN2" du L298N à la pin D8 de l'Arduino
-#define borneIN3        7       // On associe la borne "IN3" du L298N à la pin D7 de l'Arduino
-#define borneIN4        6       // On associe la borne "IN4" du L298N à la pin D6 de l'Arduino
-#define borneENB        5       // On associe la borne "ENB" du L298N à la pin D5 de l'Arduino (PWM possible sur cette broche)
+//*******************************************
+// Module L298N pour commander un
+// Moteur 2 phases pas à pas (4 fils)
+//============================================
+//Câblage du steper, utilise 6 broches
+int ENA=2; // Arduino Pin 2
+int IN1=3; // Arduino Pin 3
+int IN2=4; // Arduino Pin 4
+int ENB=5; // Arduino Pin 5
+int IN3=6; // Arduino Pin 6
+int IN4=7; // Arduino Pin 7
 
-//*************************//
-// Constantes du programme //
-//*************************//
-#define delaiChangementVitesse     20       // Défini un petit délai, exprimé en millisecondes, avant tout autre changement de vitesse
-#define vitesseMinimale            60       // Rapport cylique minimal du signal PWM, pour faire tourner le moteur au minimum de sa vitesse (en pratique, on évitera de trop approcher la valeur  0)
-#define vitesseMaximale            255      // Rapport cylique maximal du signal PWM, pour faire tourner le moteur au maximum de sa vitesse
-
-const char MARCHE_AVANT   = 'V';            // Défini une constante pour la "marche avant" (peu importe la valeur)
-const char MARCHE_ARRIERE = 'R';            // Défini une constante pour la "marche arrière" (peu importe la valeur)
-
-//************************//
-// Variables du programme //
-//************************//
-int vitesse;                    // Spécifie la vitesse de rotation du moteur, entre son minimum (0) et son maximum (255) <= signal PWM
-
-//*******//
-// SETUP //
-//*******//
 void setup() {
-  
-  // Configuration de toutes les pins de l'Arduino en "sortie" (car elles attaquent les entrées du module L298N)
-  pinMode(borneENA, OUTPUT);
-  pinMode(borneIN1, OUTPUT);
-  pinMode(borneIN2, OUTPUT);
-  pinMode(borneIN3, OUTPUT);
-  pinMode(borneIN4, OUTPUT);
-  pinMode(borneENB, OUTPUT);
+ pinMode(ENA,OUTPUT);  //Les 6 pins configurées en sorties
+ pinMode(ENB,OUTPUT);
+ pinMode(IN1,OUTPUT);
+ pinMode(IN2,OUTPUT);
+ pinMode(IN3,OUTPUT);
+ pinMode(IN4,OUTPUT);
+ //Enable A et B, utilise les 2 ponts en H
+ digitalWrite(ENA,HIGH);// Activer pont A
+ digitalWrite(ENB,HIGH);// Activer pont B
+}
+void loop(){
+ // Un moteur Pas-à-pas se contrôle à l'aide de 4 battements
+ // Séquences AB   A-B   A-B-  AB-  ou séquence  AB   AB-   A-B-   A-B
+ 
+ int tps = 20;  //Délai en ms entre deux commandes de changement de pas (vitesse du moteur)
+
 }
 
-//**************************//
-// Boucle principale : LOOP //
-//**************************//
-void loop() {
+ digitalWrite(IN1,LOW);  //0110  A-B
+ digitalWrite(IN2,HIGH);
+ digitalWrite(IN3,HIGH);
+ digitalWrite(IN4,LOW);
+ 
+ digitalWrite(IN1,LOW);  //0101  A-B-
+ digitalWrite(IN2,HIGH);
+ digitalWrite(IN3,LOW);
+ digitalWrite(IN4,HIGH);
 
-  // ***************** MARCHE AVANT ******************** //
-
-  // On configure le sens de rotation du moteur branché sur le pont A en "marche avant"
-  configurerSensDeRotationPontA(MARCHE_AVANT);
-
-  // On lance le moteur raccordé au pont A, en faisant varier sa vitesse de 0 à sa vitesse maximale (atteinte lorsque le signal PWM a un rapport cyclique égal à 255)
-  for(vitesse=vitesseMinimale ; vitesse<vitesseMaximale ; vitesse++) {
-    changeVitesseMoteurPontA(vitesse);            // Met à jour la vitesse du moteur, en modifiant le rapport cyclique du signal PWM envoyé
-    delay(delaiChangementVitesse);                // ... et on rajoute un petit délai pour que tout n'aille pas trop vite !
-  }
-
-  // Puis on ramène sa vitesse à 0, au même rythme
-  for(vitesse=vitesseMaximale ; vitesse>vitesseMinimale ; vitesse--) {
-    changeVitesseMoteurPontA(vitesse);            // Met à jour la vitesse du moteur, en modifiant le rapport cyclique du signal PWM envoyé
-    delay(delaiChangementVitesse);                // ... et on rajoute un petit délai pour que tout n'aille pas trop vite !
-  }
-
-  // ****************** PETITE PAUSE ********************* //
-  changeVitesseMoteurPontA(0);
-  delay(1000);
-  
-  // ***************** MARCHE ARRIÈRE ******************** //
-  
-  // On configure le sens de rotation du moteur branché sur le pont A en "marche arrire"
-  configurerSensDeRotationPontA(MARCHE_ARRIERE);
-
-  // On lance le moteur raccordé au pont A, en faisant varier sa vitesse de 0 à sa vitesse maximale (atteinte lorsque le signal PWM a un rapport cyclique égal à 255)
-  for(vitesse=vitesseMinimale ; vitesse<vitesseMaximale ; vitesse++) {
-    changeVitesseMoteurPontA(vitesse);            // Met à jour la vitesse du moteur, en modifiant le rapport cyclique du signal PWM envoyé
-    delay(delaiChangementVitesse);                // ... et on rajoute un petit délai pour que tout n'aille pas trop vite !
-  }
-
-  // Puis on ramène sa vitesse à 0, au même rythme
-  for(vitesse=vitesseMaximale ; vitesse>vitesseMinimale ; vitesse--) {
-    changeVitesseMoteurPontA(vitesse);            // Met à jour la vitesse du moteur, en modifiant le rapport cyclique du signal PWM envoyé
-    delay(delaiChangementVitesse);                // ... et on rajoute un petit délai pour que tout n'aille pas trop vite !
-  }
-   
-  // ****************** PETITE PAUSE ********************* //
-  // ********* ET ON RE-BOUCLE INDÉFINIMENT ! ********* //
-  changeVitesseMoteurPontA(0);
-  delay(1000);
-}
-
-
-
-//************************************************************************************//
-// Fonction : configurerSensDeRotationPontA()                                         //
-// But :      Permet de définir le sens de rotation du moteu branché sur le pont A    //
-//************************************************************************************//
-void configurerSensDeRotationPontA(char sensDeRotation) {
-
-  if(sensDeRotation == MARCHE_AVANT) {
-    // Configuration du L298N en "marche avant", pour le moteur connecté au pont A. Selon sa table de vérité, il faut que :
-    digitalWrite(borneIN1, HIGH);                 // L'entrée IN1 doit être au niveau haut
-    digitalWrite(borneIN2, LOW);                  // L'entrée IN2 doit être au niveau bas    
-  }
-  
-  if(sensDeRotation == MARCHE_ARRIERE) {
-    // Configuration du L298N en "marche arrière", pour le moteur câblé sur le pont A. Selon sa table de vérité, il faut que :
-    digitalWrite(borneIN1, LOW);                  // L'entrée IN1 doit être au niveau bas
-    digitalWrite(borneIN2, HIGH);                 // L'entrée IN2 doit être au niveau haut
-  }
-}
-
-//************************************************************************************//
-// Fonction : changeVitesseMoteurPontA()                                              //
-// But :      Change la vitesse de rotation du moteur branché sur le pont A           //
-//            (cette valeur doit être comprise entre 0 et 255)                        //
-//************************************************************************************//
-void changeVitesseMoteurPontA(int nouvelleVitesse) {
-  
-  // Génère un signal PWM permanent, de rapport cyclique égal à "nouvelleVitesse" (valeur comprise entre 0 et 255)
-  analogWrite(borneENA, nouvelleVitesse);
-}
+ digitalWrite(IN1,HIGH); //1001  AB-
+ digitalWrite(IN2,LOW);
+ digitalWrite(IN3,LOW);
+ digitalWrite(IN4,HIGH);
+ 
+ digitalWrite(IN1,HIGH);  //1010  AB
+ digitalWrite(IN2,LOW);
+ digitalWrite(IN3,HIGH);
+ digitalWrite(IN4,LOW);
+ 
