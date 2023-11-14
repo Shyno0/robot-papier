91fa50033eb87c540acc252277a44a9f482e5e72
Shynoo <146827279+Shyno0@users.noreply.github.com>
2023-11-14 09:52:39 +0100

    Update code.ino

--- code.ino
+++ code.ino
@@ -35,8 +35,8 @@ void setup() {
   pinMode(IN3, OUTPUT); //
   pinMode(IN4, OUTPUT); //
   //Enable A et B, utilise les 2 ponts en H bloque les deux roues
-  digitalWrite(ENA, HIGH); // Activer pont A
-  digitalWrite(ENB, HIGH); // Activer pont B
+  digitalWrite(ENA, LOW); // Activer pont A
+  digitalWrite(ENB, LOW); // Activer pont B
 }
 void loop() {
   //des données sur la liaison série (lorsque l'on appuie sur '1' ou '2')
@@ -44,136 +44,105 @@ void loop() {
   {
     commande = Serial.read(); //on lit
     
-    if (commande == '1') // le robot ferme la pince lorsqu'il recoit 1
+    if (commande == '1')
     {
-      Serial.print("position pince fermé  : "); // écrit le texte " "
-      Serial.println(ServoClose); // écrit " la valeur de ServoClose "
-      ServoMoteur.writeMicroseconds(ServoClose); // la pince se ferme
+      Serial.print("position pince fermé  : ");
+      Serial.println(ServoClose);
+      ServoMoteur.writeMicroseconds(ServoClose);
     }
-    else if (commande == '2') // le robot ouvre la pince lorsqu'il reçoit 2
+    else if (commande == '2')
     {
-      Serial.print("position pince ouvert : "); // écrit le texte " "
-      Serial.println(ServoOpen); // écrit " La valeur de ServoOpen "
-      ServoMoteur.writeMicroseconds(ServoOpen); // la pince s'ouvre
+      Serial.print("position pince ouvert : ");
+      Serial.println(ServoOpen);
+      ServoMoteur.writeMicroseconds(ServoOpen);
     }
-    else if (commande == '3') // le robot avance lorsqu'il reçoit "3"
+    else if (commande == '3')
     {
-      avant();   // le robot avance
-      Serial.println("Robot avance"); // écrit le texte " "
-      delay(10); // délai de 10 microseconde
+      avant();   // le robot avance lorsqu'il reçoit "3"
+      Serial.println("Robot avance");
+      delay(10);
     }
-    else if (commande == '4') // le robot recule lorsqu'il reçoit "4"
+    else if (commande == '4')
     {
-      arriere(); // le robot recule
-      Serial.println("Robot recule"); // écrit le texte " "
-      delay(10); // délai de 10 microseconde
+      arriere(); // le robot recule lorsqu'il reçoit "4"
+      Serial.println("Robot recule");
+      delay(10);
     }
-    else if (commande == '5') // le robot tourne a gauche lorsqu'il reçoit "5"
+    else if (commande == '5')
     {
-      gauche();  // le robot tourne a gauche 
-      Serial.println("Robot tourne a gauche"); // écrit le texte " "
-      delay(10); // délai de 10 microseconde
+      gauche();  // le robot tourne a gauche lorsqu'il reçoit "5"
+      Serial.println("Robot tourne a gauche");
+      delay(10);
     }
-    else if (commande == '6') // le robot tourne a droite lorsqu'il reçoit "6"
+    else if (commande == '6')
     {
-      droite();  // le robot tourne a droite
-      Serial.println("Robot tourne a droite"); // écrit le texte " "
-      delay(10); // délai de 10 microseconde
+      droite();  // le robot tourne a droite lorsqu'il reçoit "6"
+      Serial.println("Robot tourne a droite");
+      delay(10);
     }
-    else if (commande == '7') // le robot se stop lorsqu'il ne reçoit rien ( 7 pour les test)
+    else if (commande == '7')
     {
-      stop1();  // le robot se stop
+      stop1();  // le robot se stop lorsqu'il ne reçoit rien
       Serial.println("Robot ne bouge pas");
-      delay(10); // délai de 10 microseconde
+      delay(10);
     }
   }
 }
 
 void avant () // programme pour que le robot avance
 {
-  digitalWrite(IN1, HIGH); //broche IN1 alimenté
-  digitalWrite(IN2, LOW);  //broche IN2 pas alimenté
-  digitalWrite(IN3, HIGH); //broche IN3 alimenté
-  digitalWrite(IN4, LOW);  //broche IN4 pas alimenté
+  digitalWrite(IN1, HIGH); // broche IN1 alimenté
+  digitalWrite(IN2, LOW);  // broche IN2 pas alimenté
+  digitalWrite(IN3, HIGH); // broche IN3 alimenté
+  digitalWrite(IN4, LOW);  // broche IN4 pas alimenté
+  delay(1);
+  digitalWrite(ENA, HIGH); // Activer pont A
+  digitalWrite(ENB, HIGH); // Activer pont B
 }
 
 void arriere () // programme pour que le robot recule
 {
-  digitalWrite(IN1, LOW);  //broche IN1 pas alimenté
-  digitalWrite(IN2, HIGH); //broche IN2 alimenté
-  digitalWrite(IN3, LOW);  //broche IN3 pas alimenté
-  digitalWrite(IN4, HIGH); //broche IN4 alimenté
+  digitalWrite(IN1, LOW);  // broche IN1 pas alimenté
+  digitalWrite(IN2, HIGH); // broche IN2 alimenté
+  digitalWrite(IN3, LOW);  // broche IN3 pas alimenté
+  digitalWrite(IN4, HIGH); // broche IN4 alimenté
+  delay(1);
+  digitalWrite(ENA, HIGH); // Activer pont A
+  digitalWrite(ENB, HIGH); // Activer pont B
 }
 
 void stop1 () // programme pour que le robot se stop
 {
-  digitalWrite(IN1, HIGH); //broche IN1 alimenté
-  digitalWrite(IN2, HIGH); //broche IN2 alimenté
-  digitalWrite(IN3, HIGH); //broche IN3 alimenté
-  digitalWrite(IN4, HIGH); //broche IN4 alimenté
+  //digitalWrite(IN1, LOW); // broche IN1 pas alimenté
+  //digitalWrite(IN2, LOW); // broche IN2 pas alimenté
+  //digitalWrite(IN3, LOW); // broche IN3 pas alimenté
+  //digitalWrite(IN4, LOW); // broche IN4 pas alimenté 
+  
+  digitalWrite(ENA, LOW); // Désactiver pont A
+  digitalWrite(ENB, LOW); // Désactiver pont B
 }
 
 void gauche () // programme pour que le robot tourne a gauche
 {
-  digitalWrite(IN1, HIGH); // vitesse A CHANGER ET RAJOUTER
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH); // vitesse A CHANGER ET RAJOUTER
-  digitalWrite(IN4, LOW);
+  //digitalWrite(IN1, LOW);  // broche IN1 pas alimenté
+  //digitalWrite(IN2, LOW);  // broche IN2 pas alimenté
+  digitalWrite(IN3, HIGH); // broche IN3 alimenté vitesse A CHANGER ET RAJOUTER ?
+  digitalWrite(IN4, LOW);  // broche IN4 pas alimenté
+  delay(1);
+  digitalWrite(ENA, LOW);  // Désactiver pont A
+  digitalWrite(ENB, HIGH); // Activer pont B
 }
 
 void droite () // programme pour que le robot tourne a gauche
 {
-  digitalWrite(IN1, HIGH); // vitesse A CHANGER ET RAJOUTER
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH); // vitesse A CHANGER ET RAJOUTER
-  digitalWrite(IN4, LOW);
+  digitalWrite(IN1, HIGH); // broche IN1 alimenté vitesse A CHANGER ET RAJOUTER ?
+  digitalWrite(IN2, LOW);  // broche IN2 pas alimenté
+  //digitalWrite(IN3, LOW);  // broche IN3 pas alimenté
+  //digitalWrite(IN4, LOW);  // broche IN4 pas alimenté
+  delay(1);
+  digitalWrite(ENA, HIGH); // Activer pont A
+  digitalWrite(ENB, LOW);  // Désactiver pont A
 }
-
-// A RETIRER POTENTIELLEMENT
-
-void stop2 () // second programme pour que le robot se stop
-{
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-}
-
-void mesureDirection () // ecrit les mouvement du robot
-{
-
-  if      (commande == '3')   // le robot avance lorsqu'il reçoit "3"
-  {
-    Serial.println("Robot avance");
-    delay(10);
-  }
-
-  else if (commande == '4')   // le robot recule lorsqu'il reçoit "4"
-  {
-    Serial.println("Robot recule");
-    delay(10);
-  }
-
-  else if (commande == '5')    // le robot s'arrete lorsqu'il reçoit "5"
-  {
-    Serial.println("Robot tourne a gauche");
-    delay(10);
-  }
-
-  else if (commande == '6')   // le robot tourne a gauche lorsqu'il reçoit "6"
-  {
-    Serial.println("Robot tourne a droite");
-    delay(10);
-  }
-
-  else if (commande == '7') // le robot tourne a droite droite lorsqu'il ne reçoit rien
-  {
-    Serial.println("Robot ne bouge pas");
-    delay(10);
-  }
-}
-
-// RETIRER POTENTIELLEMENT
 
 //*******************************************
 // Commentaire programme
